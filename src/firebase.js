8fb7c36ec53a234b65191d0355801692f7c3f203
RyedzOffc <ryedzoffc@gmail.com>
2026-04-13 12:03:38 +0800

    Initialize Firebase app and set up authentication

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4LmonPFC7CfjD3Y3xZbVsUWr-WEKrN0c",
+  authDomain: "ryedzoffc-2d40f.firebaseapp.com",
+  projectId: "ryedzoffc-2d40f",
+  storageBucket: "ryedzoffc-2d40f.firebasestorage.app",
+  messagingSenderId: "1053013297416",
+  appId: "1:1053013297416:web:07741babea215a8bb5c4bd"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
